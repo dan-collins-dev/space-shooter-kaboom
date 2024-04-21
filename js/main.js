876c90ff418325b899efea0655f6d1348b8bf3f6
Dan Collins <dan.collins.dev@gmail.com>
2024-04-21 18:34:50 -0400

    refactored code for containing multiple scenes

--- js/main.js
+++ js/main.js
@@ -13,95 +13,126 @@ kaboom({
     canvas: document.querySelector("#mycanvas"),
     background: [0, 0, 0],
     debug: true,
+    crisp: true,
     // scale: 2
 });
 
-// Loading the player sprite
+// Loading the sprites
+// NOTE: Down the line look into how to importing asesprite files work
 loadSprite("playerShip", "./assets/sprites/playerShip.png");
 loadSprite("laser", "./assets/sprites/laser.png");
 loadSprite("enemyShip", "./assets/sprites/enemyShip.png");
 
-const player = add([
-    sprite("playerShip"),
-    scale(3),
-    anchor("center"),
-    pos(center().x, gameHeight - 40),
-    area(),
-    "player",
-    {
-        speed: 240,
-        offset: 24,
-    },
-    health(3),
-]);
+// Load font
+loadFont("PressStart2P", "./assets/fonts/PressStart2P-Regular.ttf")
 
-const spawnLaser = (playerPos) => {
-    add([
-        sprite("laser"),
-        scale(2),
+// Main Menu Scene
+let mainMenu = scene("MainMenu", () => {
+    const titleMsg = add([
+        text("TEST", {
+            size: 32,
+            font: "PressStart2P"
+        }),
         anchor("center"),
-        pos(playerPos.x, playerPos.y - 25),
-        area(),
-        "projectile",
-        {
-            onCoolDown: false,
-        },
-        move(UP, 200),
-        fixed(),
-    ]);
-};
+        pos(center().x, 160),
+    ])
+})
 
-const spawnEnemy = () => {
-    loop(enemySpawnDelay, () => {
+
+
+// Main Gameplay Scene
+let gameplay = scene("Game", () => {
+    const player = add([
+        sprite("playerShip"),
+        scale(3),
+        anchor("center"),
+        pos(center().x, gameHeight - 40),
+        area(),
+        "player",
+        {
+            speed: 240,
+            offset: 24,
+        },
+        health(3),
+    ]);
+
+    const spawnLaser = (playerPos) => {
         add([
-            sprite("enemyShip"),
-            scale(3),
+            sprite("laser"),
+            scale(2),
             anchor("center"),
-            pos(randi(24, gameWidth - 24), -200),
+            pos(playerPos.x, playerPos.y - 25),
             area(),
-            body(),
-            "enemy",
+            "projectile",
             {
-                speed: 240,
-                offset: 24,
+                onCoolDown: false,
             },
-            move(DOWN, randi(100, 150)),
+            move(UP, 200),
             fixed(),
         ]);
+    };
+
+    const spawnEnemy = () => {
+        loop(enemySpawnDelay, () => {
+            let e = add([
+                sprite("enemyShip"),
+                scale(3),
+                anchor("center"),
+                pos(randi(24, gameWidth - 24), -200),
+                area(),
+                body(),
+                "enemy",
+                {
+                    speed: 240,
+                    offset: 24,
+                },
+                move(DOWN, randi(100, 150)),
+                fixed(),
+            ]);
+            e.onCollide("projectile", (p) => {
+                destroy(e);
+                score += 1;
+                console.log(score)
+                destroy(p);
+            });
+        });
+    };
+
+    onKeyDown("a", () => {
+        if (player.pos.x <= player.offset) player.pos.x = player.offset;
+        else player.move(-player.speed, 0);
     });
-};
 
-onKeyDown("a", () => {
-    if (player.pos.x <= player.offset) player.pos.x = player.offset;
-    else player.move(-player.speed, 0);
+    onKeyDown("d", () => {
+        if (player.pos.x >= gameWidth - player.offset)
+            player.pos.x = gameWidth - player.offset;
+        else player.move(player.speed, 0);
+    });
+
+    onKeyRelease("space", () => {
+        if (!player.onCoolDown) {
+            player.onCoolDown = true;
+            spawnLaser(player.pos);
+        } else {
+            wait(fireDelay, () => (player.onCoolDown = false));
+        }
+    });
+
+    player.onCollide("enemy", (e) => {
+        shake(dmgShakeFactor);
+        player.hurt(1);
+    });
+
+    player.on("hurt", () => {
+        console.log("OUCH");
+    });
+
+    player.on("death", () => {
+        destroy(player);
+    });
+
+    spawnEnemy();
 });
 
-onKeyDown("d", () => {
-    if (player.pos.x >= gameWidth - player.offset)
-        player.pos.x = gameWidth - player.offset;
-    else player.move(player.speed, 0);
-});
-
-onKeyRelease("space", () => {
-    if (!player.onCoolDown) {
-        player.onCoolDown = true;
-        spawnLaser(player.pos);
-    } else {
-        wait(fireDelay, () => (player.onCoolDown = false));
-    }
-});
-
-player.onCollide("enemy", (e) => {
-    shake(dmgShakeFactor);
-    player.hurt(1);
-});
-
-player.on("hurt", () => {
-    console.log("OUCH");
-});
-
-player.on("death", () => {
-    destroy(player);
-});
-
-spawnEnemy();
+// go("MainMenu");
+go("Game")
